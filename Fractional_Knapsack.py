11e42f2b831ea031e6f9b3172224821f95ac380d
Devjyoti29 <52920257+Devjyoti29@users.noreply.github.com>
2022-11-19 10:42:19 +0530

    Create Fractional_Knapsack.py

---
+++ Fractional_Knapsack.py
@@ -0,0 +1,27 @@
+item=[1000,600,900]
+profit=[100,50,30]
+
+c=120
+
+arr=[]
+for i in range(0,len(item)):
+    it=item[i]
+    pt=profit[i]
+    pkg=pt/it
+    arr.append([it,pt,pkg])
+
+arr.sort(key=lambda x:x[2])
+arr=arr[::-1]
+
+i=0
+profit=0
+while(c>0):
+    if arr[i][0]<=c:
+        c=c-arr[i][0] #positive
+        profit+=arr[i][1]
+    else:
+        profit+=c*arr[i][2]
+        c=0
+    i+=1
+
+print(profit)
